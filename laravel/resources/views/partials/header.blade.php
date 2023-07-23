4defa1abafbc484c8d208cb7c3d76cd4da10daaf
Anieb22 <anielloverdino@gmail.com>
2023-07-23 19:14:23 +0200

    creato partial header

---
+++ laravel/resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <div class="bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex flex-row-reverse">
+                    <div class="col-2 text-light text-uppercase">Additional dc site &#x25BC;</div>
+                    <div class="col-2 text-light text-uppercase">DC POWER&#8482; VISA&#174;</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <div class="col-2 my-3">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
+                    </div>
+                    <div class="col-9">
+                        <ul class="list-group list-group-horizontal justify-content-end d-flex">
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Characters</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Comics</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Movies</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">TV</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Games</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Collectibles</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Videos</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Fans</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">News</li>
+                            <li class="list-group-item border border-0 text-uppercase bg-transparent">Shop <i class="fas fa-caret-down"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
