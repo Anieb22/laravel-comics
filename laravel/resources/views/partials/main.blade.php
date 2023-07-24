52c6fcb7756aadced266ecf673168b66ef238288
Anieb22 <anielloverdino@gmail.com>
2023-07-24 19:54:05 +0200

    aggiunto font awesome

--- laravel/resources/views/partials/main.blade.php
+++ laravel/resources/views/partials/main.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
         @foreach($comics as $comic)
-        <div class="col-2 my-4">
+        <div class="col-2 my-4" id="comics">
             <a href="#">
                 <div class="card border border-0 text-uppercase bg-transparent" style="width: 13rem;">
                     <img src="{{ $comic['thumb'] }}"  class="card-img-top">
@@ -13,6 +13,9 @@
             </a>
         </div>
         @endforeach
+        <div class="col-12 d-flex justify-content-center mb-4">
+            <button class="btn btn-primary text-uppercase px-5 border border-0 rounded-0">load more</button>
+        </div>
         </div>
     </div>
 </main>
